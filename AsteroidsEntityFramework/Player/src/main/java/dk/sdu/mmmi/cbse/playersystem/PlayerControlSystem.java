00230cc5fd84afc999c55affa0de5f10f5c8a222
Bende Siewertsen <bende1308@gmail.com>
2020-02-19 22:27:06 +0100

    class diagram

--- AsteroidsEntityFramework/Player/src/main/java/dk/sdu/mmmi/cbse/playersystem/PlayerControlSystem.java
+++ AsteroidsEntityFramework/Player/src/main/java/dk/sdu/mmmi/cbse/playersystem/PlayerControlSystem.java
@@ -2,13 +2,13 @@ package dk.sdu.mmmi.cbse.playersystem;
 
 import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
-import static dk.sdu.mmmi.cbse.common.data.GameKeys.LEFT;
-import static dk.sdu.mmmi.cbse.common.data.GameKeys.RIGHT;
-import static dk.sdu.mmmi.cbse.common.data.GameKeys.UP;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
 import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.WeaponPart;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+
+import static dk.sdu.mmmi.cbse.common.data.GameKeys.*;
 
 /**
  *
@@ -22,14 +22,16 @@ public class PlayerControlSystem implements IEntityProcessingService {
         for (Entity player : world.getEntities(Player.class)) {
             PositionPart positionPart = player.getPart(PositionPart.class);
             MovingPart movingPart = player.getPart(MovingPart.class);
+            WeaponPart weaponPart = player.getPart(WeaponPart.class);
 
             movingPart.setLeft(gameData.getKeys().isDown(LEFT));
             movingPart.setRight(gameData.getKeys().isDown(RIGHT));
             movingPart.setUp(gameData.getKeys().isDown(UP));
-            
+            weaponPart.setShooting(gameData.getKeys().isDown(SPACE));
             
             movingPart.process(gameData, player);
             positionPart.process(gameData, player);
+            weaponPart.process(gameData, player);
 
             updateShape(player);
         }
@@ -49,7 +51,7 @@ public class PlayerControlSystem implements IEntityProcessingService {
         shapex[1] = (float) (x + Math.cos(radians - 4 * 3.1415f / 5) * 8);
         shapey[1] = (float) (y + Math.sin(radians - 4 * 3.1145f / 5) * 8);
 
-        shapex[2] = (float) (x + Math.cos(radians + 3.1415f) * 5);
+        shapex[2] = (float) (x + Math.cos(radians + 3.1415f) * 6);
         shapey[2] = (float) (y + Math.sin(radians + 3.1415f) * 5);
 
         shapex[3] = (float) (x + Math.cos(radians + 4 * 3.1415f / 5) * 8);
